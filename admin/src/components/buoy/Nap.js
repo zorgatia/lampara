b44f3ce3249fe25156fd3a4f5f6643cd0a4dfb58
Zorgati <achrafmounir.zorgati@esprit.tn>
2019-09-20 11:02:50 +0200

    mobile3

--- admin/src/components/buoy/Nap.js
+++ admin/src/components/buoy/Nap.js
@@ -1,60 +1,69 @@
-import React, { Fragment, useEffect ,useState} from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import GoogleMapReact from "google-map-react";
 
 import { connect } from "react-redux";
 import { getBuoys } from "../../actions/buoy";
-import {MY_API_KEY} from '../../utils/keys'
+import { MY_API_KEY } from "../../utils/keys";
 import Spinner from "../layout/Spinner";
 
 const Nap = ({ getBuoys, buoy: { buoys, loading } }) => {
-    useEffect(() => {
-       // console.log(buoys)
-        getBuoys();
-      }, [getBuoys]);
+  useEffect(() => {
+    // console.log(buoys)
+    getBuoys();
+  }, [getBuoys]);
 
-    const [center] = useState({
-        lat: 36.81897,
-        lng: 10.16579
+  const [center] = useState({
+    lat: 36.81897,
+    lng: 10.16579
+  });
+  const [zoom] = useState(11);
+  const handleApiLoaded = (map, maps) => {
+    // let marker = new maps.Marker({ map: map, position: maps.LatLng(center) });
+    // const geocoder = new maps.Geocoder();
+    const infowindow = new maps.InfoWindow();
+
+    //let autocomplete = new maps.places.Autocomplete(searchInput.current, {types: ['geocode']})
+    setMarkers(map, maps);
+  };
+  const setMarkers = (map, maps) => {
+    const image = {
+      url:
+        "https://res.cloudinary.com/orange-odc/image/upload/v1568969768/utils/buoy_2_rckag0.png",
+      // This marker is 20 pixels wide by 32 pixels high.
+      size: new maps.Size(32, 32),
+      // The origin for this image is (0, 0).
+      origin: new maps.Point(0, 0),
+      // The anchor for this image is the base of the flagpole at (0, 32).
+      anchor: new maps.Point(0, 32)
+    };
+    const shape = {
+      coords: [1, 1, 1, 20, 18, 20, 18, 1],
+      type: "poly"
+    };
+    buoys.forEach(b => {
+      //  console.log('asd')
+      // console.log(b)
+      const marker = new maps.Marker({
+        position: { lat: b.lat, lng: b.lng },
+        map: map,
+        icon: image,
+
+        title: "test",
+        zIndex: 10
       });
-      const [zoom] = useState(11);
-      const handleApiLoaded = (map, maps) => {
-       // let marker = new maps.Marker({ map: map, position: maps.LatLng(center) });
-       // const geocoder = new maps.Geocoder();
-        const infowindow = new maps.InfoWindow();
-    
-        //let autocomplete = new maps.places.Autocomplete(searchInput.current, {types: ['geocode']})
-        setMarkers(map,maps)
-      };
-      const setMarkers = (map,maps)=>{
-        const image = {
-            url: 'https://res.cloudinary.com/orange-odc/image/upload/v1568968076/utils/buoy_jjdisu.png',
-            // This marker is 20 pixels wide by 32 pixels high.
-            size: new maps.Size(20, 32),
-            // The origin for this image is (0, 0).
-            origin: new maps.Point(0, 0),
-            // The anchor for this image is the base of the flagpole at (0, 32).
-            anchor: new maps.Point(0, 32)
-          };
-          const shape = {
-            coords: [1, 1, 1, 20, 18, 20, 18, 1],
-            type: 'poly'
-          };
-          buoys.forEach(b => {
-            //  console.log('asd')
-            // console.log(b)
-            const marker = new maps.Marker({
-                position: {lat: b.lat, lng:b.lng},
-                map: map,
-                icon: image,
-                shape: shape,
-                title: 'test',
-                zIndex: 10
-                
-              });
-          });
-      }
-  return loading || buoys==null ? <Spinner></Spinner>: (
+      marker.addListener("click", function() {
+        const infowindow=new maps.InfoWindow({
+          content: b.status,
+          maxWidth: 200
+        });
+        infowindow.open(map, marker);
+      });
+    });
+  };
+  return loading || buoys == null ? (
+    <Spinner></Spinner>
+  ) : (
     <Fragment>
       <div className="content-body">
         <div className="container">
@@ -65,9 +74,7 @@ const Nap = ({ getBuoys, buoy: { buoys, loading } }) => {
               defaultZoom={zoom}
               yesIWantToUseGoogleMapApiInternals
               onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-            >
-                
-            </GoogleMapReact>
+            ></GoogleMapReact>
           </div>
         </div>
       </div>
