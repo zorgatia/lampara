0de6f93c694380fbc1fa00eb021404a5fc0e6982
Zorgati <achrafmounir.zorgati@esprit.tn>
2019-10-06 20:03:18 +0200

    final1

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.use('/mob/user',require('./routes/mob/user'));
 app.use('/mob/auth',require('./routes/mob/auth'));
 app.use('/mob/plage',require('./routes/mob/plage'));
 app.use('/mob/event',require('./routes/mob/event'));
+app.use('/mob/rate',require('./routes/mob/rate'));
 
 
 app.use('/web/plage',require('./routes/web/plage'));
